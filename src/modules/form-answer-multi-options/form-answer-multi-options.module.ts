d94e41f780e8136078d34f990e7b0556bc8e567a
SandroGreenn <sandro@greenn.com.br>
2025-11-08 17:21:10 -0300

    ajuste import form answeres

--- src/modules/form-answer-multi-options/form-answer-multi-options.module.ts
+++ src/modules/form-answer-multi-options/form-answer-multi-options.module.ts
@@ -1,14 +1,14 @@
 import { FormAnswerMultiOption } from '@/entities/form-answer-multi-option.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { FormAnswerModule } from '../form-answers/form-answers.module';
+import { FormAnswersModule } from '../form-answers/form-answers.module';
 import { FormAnswerMultiOptionsController } from './form-answer-multi-options.controller';
 import { FormAnswerMultiOptionsService } from './form-answer-multi-options.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([FormAnswerMultiOption]),
-    FormAnswerModule,
+    FormAnswersModule,
   ],
   controllers: [FormAnswerMultiOptionsController],
   providers: [FormAnswerMultiOptionsService],
